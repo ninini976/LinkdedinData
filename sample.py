55b6bedd9894589de188983f3f7c6e8f72cba1ec
ninini976 <yangtony@umich.edu>
2016-10-26 21:39:59 -0700

    write some functions

--- sample.py
+++ sample.py
@@ -58,53 +58,58 @@ class person(object):
 		for element in self.explist:
 			element.output()
 
+def login(driver, emailadd, pswd):
+	driver.get("https://www.linkedin.com/")
+	time.sleep(1)
+	email = driver.find_element_by_id('login-email')
+	email.send_keys(emailadd)
+	time.sleep(1)
+	password = driver.find_element_by_id('login-password')
+	password.send_keys(pswd)
+	password.send_keys(Keys.RETURN)
+
+def search(driver, keyword):
+	mainsearchbox = driver.find_element_by_id('main-search-box')
+	mainsearchbox.send_keys(keyword)
+	time.sleep(2)
+	searchbutton = driver.find_element(By.XPATH, '//button[@class="search-button"]')
+	searchbutton.click()
+	time.sleep(2)
+	firstperson = driver.find_element(By.XPATH, "//li[@class=\"mod result idx1 people\"]").find_element(By.XPATH,'.//a[@class="title main-headline"]')
+	firstperson.click()
+	time.sleep(2)
+
+def fetchdata(driver, target):
+	bgedu = driver.find_element_by_id('background-education')
+	edulist = bgedu.find_elements(By.XPATH, './div')
+
+	#TODO: needs to handle some exception when certain information is not provided
+
+	for element in edulist:
+		schoolName = element.find_element(By.XPATH, './/h4').text
+		degree = element.find_element(By.XPATH, './/h5/span[@class="degree"]').text
+		fieldOfStudy = element.find_element(By.XPATH, './/h5/span[@class="major"]').text
+		date = element.find_element(By.XPATH, './/span[@class="education-date"]').text
+		target.addedurecord(education(schoolName, degree, fieldOfStudy, date))
+
+	bgexp = driver.find_element_by_id('background-experience')
+	bglist = bgexp.find_elements(By.XPATH, './div')
+	for element in bglist:
+		company = element.find_element(By.XPATH, './/header/h5[not(@*)]').text
+		title = element.find_element(By.XPATH, './/header/h4').text
+		date = str(element.find_element(By.XPATH, './/span[@class="experience-date-locale"]/time[1]').text) + ' - ' + str(element.find_element(By.XPATH, './/span[@class="experience-date-locale"]/time[2]').text)
+		target.addexprecord(experience(company, title, date))
 
 target = person("Jinlei")
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.linkedin.com/")
-time.sleep(1)
-email = driver.find_element_by_id('login-email')
-email.send_keys("yangtony@umich.edu")
-time.sleep(1)
-password = driver.find_element_by_id('login-password')
-password.send_keys("nnsshxsssnnrbt")
+login(driver, '482655720@qq.com', '1a2b3c4dD')
+search(driver, 'xuefeng hu')
+fetchdata(driver, target)
 
-password.send_keys(Keys.RETURN)
-
-mainsearchbox = driver.find_element_by_id('main-search-box')
-mainsearchbox.send_keys("jinlei chen")
-time.sleep(2)
-mainsearchbox.send_keys(Keys.ARROW_DOWN)
-mainsearchbox.send_keys(Keys.RETURN)
-
-time.sleep(2)
-
-bgedu = driver.find_element_by_id('background-education')
-edulist = bgedu.find_elements(By.XPATH, './div')
-
-# TODO: needs to handle some exception when certain information is not provided
-
-for element in edulist:
-	schoolName = element.find_element(By.XPATH, './/h4').text
-	degree = element.find_element(By.XPATH, './/h5/span[@class="degree"]').text
-	fieldOfStudy = element.find_element(By.XPATH, './/h5/span[@class="major"]').text
-	date = element.find_element(By.XPATH, './/span[@class="education-date"]').text
-	target.addedurecord(education(schoolName, degree, fieldOfStudy, date))
-
-bgexp = driver.find_element_by_id('background-experience')
-bglist = bgexp.find_elements(By.XPATH, './div')
-for element in bglist:
-	company = element.find_element(By.XPATH, './/header/h5[not(@*)]').text
-	title = element.find_element(By.XPATH, './/header/h4').text
-	date = str(element.find_element(By.XPATH, './/span[@class="experience-date-locale"]/time[1]').text) + ' - ' + str(element.find_element(By.XPATH, './/span[@class="experience-date-locale"]/time[2]').text)
-	target.addexprecord(experience(company, title, date))
 
 target.output()
-
-#target.output()
-
 
 time.sleep(3)
 
