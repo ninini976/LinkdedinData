a632589628824cf6fce4fa4d4b9a551d2d0e0600
ninini976 <yangtony@umich.edu>
2016-11-08 15:26:46 -0800

    all fields required, start output json

--- sample.py
+++ sample.py
@@ -1,12 +1,14 @@
 # from pyvirtualdisplay import Display
 from selenium import webdriver
-
+import json
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 from selenium.common.exceptions import NoSuchElementException
 # display = Display(visible=0, size=(1024, 768))
 # display.start()
+
+import re
 
 
 class education(object):
@@ -23,6 +25,22 @@ class education(object):
 		print("fieldOfStudy: " + self.fieldOfStudy)
 		print("date: " + self.date +'\n')
 
+	def outputstr(self):
+		string = ''
+		string += ("schoolName: " + self.schoolName + '\n')
+		string += ("degree: " + self.degree + '\n')
+		string += ("fieldOfStudy: " + self.fieldOfStudy + '\n')
+		string += ("date: " + self.date +'\n')
+		return string
+
+	def returnobject(self):
+		return {
+			"Date": self.date,
+			"Degree": self.degree,
+			"Field of study": self.fieldOfStudy,
+			"School name": self.schoolName
+		}
+
 class experience(object):
 	"""experience class contain the company, title and date of an experience record"""
 	def __init__(self, company, title, date):
@@ -35,14 +53,28 @@ class experience(object):
 		print("title: " + self.title)
 		print("date: " + self.date +'\n')
 
+	def outputstr(self):
+		string = ''
+		string += ("company: " + self.company + '\n')
+		string += ("title: " + self.title + '\n')
+		string += ("date: " + self.date +'\n')
+		return string
 
+	def returnobject(self):
+		return {
+			"Company": self.company,
+			"Date": self.date,
+			"Title": self.title
+		}
 
 class person(object):
 	"""person class contains the name of a person, a list of education background and a list experience """
 	def __init__(self, name):
 		self.name = name
+		self.industry = ''
 		self.edulist = []
 		self.explist = []
+		self.url = ''
 		self.info = ''
 
 	def addedurecord(self, edurecord):
@@ -54,14 +86,48 @@ class person(object):
 	def addinfo(self, info):
 		self.info = info
 
+	def addindstry(self, industry):
+		self.industry = industry
+
+	def addurl(self, url):
+		self.url = url
+
 	def output(self):
 		print("Name of the person:" + self.name)
+		print("Industry:" + self.industry)
 		print("Education background:")
 		for element in self.edulist:
 			element.output()
 		print("Experience list:")
 		for element in self.explist:
 			element.output()
+
+	def outputstr(self):
+		string = ''
+		string += ("Name of the person:" + self.name + '\n')
+		string += ("Industry:" + self.industry + '\n')
+		string += ("Education background:" + '\n')
+		for element in self.edulist:
+			string += element.outputstr()
+		string += ("Experience list:" + '\n')
+		for element in self.explist:
+			string += element.outputstr()
+		return string
+
+	def returnobj(self):
+		eduvec = []
+		for edu in self.edulist:
+			eduvec.append(edu.returnobject())
+		expvec = []
+		for exp in self.explist:
+			expvec.append(exp.returnobject())
+		return {
+			"Education": eduvec,
+			"Experience": expvec,
+			"Industry": self.industry,
+			"Name": self.name,
+			"Target URL": self.url
+		}
 
 def login(driver, emailadd, pswd):
 	driver.get("https://www.linkedin.com/")
@@ -108,6 +174,15 @@ def fetchdata(driver, target):
 	except NoSuchElementException:
 		time.sleep(1)
 
+	try:
+		industry = driver.find_element(By.XPATH, '//dd[@class="industry"]').text
+		target.addindstry(industry)
+	except NoSuchElementException:
+		industry = ''
+
+	url = driver.current_url
+	url = url[:url.index('?')]
+	target.addurl(url)
 
 	try:
 		bgedu = driver.find_element_by_id('background-education')
@@ -129,7 +204,12 @@ def fetchdata(driver, target):
 			except NoSuchElementException:
 				fieldOfStudy = ''
 			try:
-				date = element.find_element(By.XPATH, './/span[@class="education-date"]').text
+				datelist = element.find_elements(By.XPATH, './/span[@class="education-date"]/time')
+				if len(datelist) == 2:
+					date = str(element.find_element(By.XPATH, './/span[@class="education-date"]/time[1]').text) + ' ' + element.find_element(By.XPATH, './/span[@class="education-date"]/time[2]').text
+					date = date[:4] + ' - ' + date[-4:]
+				else:
+					date = str(element.find_element(By.XPATH, './/span[@class="education-date"]/time[1]').text) + ' - ' + 'present'
 			except NoSuchElementException:
 				date = ''
 			target.addedurecord(education(schoolName, degree, fieldOfStudy, date))
@@ -160,18 +240,46 @@ def fetchdata(driver, target):
 	except NoSuchElementException:
 		print("No background experience found for the target")
 
-driver = webdriver.Firefox()
+#driver = webdriver.Firefox()
+#login(driver, '482655720@qq.com', '1a2b3c4dD')
 
+
+# target = person("Men-Chow Chiang")
+# if search(driver, 'Men-Chow Chiang'):	
+# 	fetchdata(driver, target)
+# else:
+# 	print("failed to find the target, login fail or no result found")
+# target.output()
+
+f = open('input_data_10000.txt','r')
+
+name_school_list = []
+for line in f:
+	# remove the last '\n' in each line
+	line = line[:-1]
+	pattern = "\'(.*)\',\'(.*)\'"
+	name, school = re.match(pattern, line).group(1,2)
+	name_school_list.append((name, school))
+
+f.close()
+f = open("output.txt","w")
+
+driver = webdriver.Firefox()
 login(driver, '482655720@qq.com', '1a2b3c4dD')
 
+# for i in range(5):
+# 	target = person(name_school_list[i][0])
+# 	if search(driver, name_school_list[i][0]):	
+# 		fetchdata(driver, target)
+# 	else:
+# 		print("failed to find the target, login fail or no result found")
+# 	print(target.outputstr())	
 
-target = person("Men-Chow Chiang")
-if search(driver, 'Men-Chow Chiang'):	
+target = person("Yulin Xie")
+if search(driver, "Yulin Xie"):	
 	fetchdata(driver, target)
 else:
 	print("failed to find the target, login fail or no result found")
-target.output()
+print(target.outputstr())
 
-
-time.sleep(3)
-
+print(json.dumps(target.returnobj()))
